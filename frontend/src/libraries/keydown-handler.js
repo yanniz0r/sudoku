7e1d479e1ea743d1a5dc453fe80f84dbc698e188
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-02-15 11:33:59 +0100

    Removes debug code from keydown handler

--- frontend/src/libraries/keydown-handler.js
+++ frontend/src/libraries/keydown-handler.js
@@ -3,7 +3,6 @@ import { setValue } from '../redux/actions/values'
 
 const keydownHandler = (event) => {
   const intValue = parseInt(event.key)
-  window.i = intValue
   if(Number.isInteger(intValue) && 0 < intValue && intValue <= 9) {
     const { x, y, isAvailable } = store.getState().selectedCell
     if(isAvailable) {
